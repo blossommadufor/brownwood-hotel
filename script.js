29918639d8a0c8cc410424f4a2b65f9309da24b2
Christian Madufor <chrismadufor@gmail.com>
2023-11-30 21:08:16 +0100

    edit

---
+++ script.js
@@ -0,0 +1,69 @@
+// SELECTORS
+
+const menuIconBox = document.querySelector(".menu_icon_box");
+
+const Box =document.querySelector(".box");
+
+
+
+// EVENT LISTENERS
+
+menuIconBox.addEventListener('click', showList);
+
+// menu_icon_box.onclick =
+
+
+// FUNCTION
+
+
+function showList(){
+        Box.classList.toggle("active");
+}
+
+
+
+
+
+
+
+// menuIconBox.classList.toggle("active");
+
+
+
+
+
+
+
+
+
+
+
+
+// // SELECTORS
+// const sideButton = document.querySelector(".hamburger");
+// const navBar = document.querySelector(".nav-bar")
+
+
+
+// // EVENT LISTENERS
+
+// sideButton.addEventListener('click', showList);
+
+// // 
+
+
+
+// // FUNCTIONS
+
+// function showList(){
+    
+//     navBar.style.display = "block"
+
+//     // if( sideButton.clicked === true){
+//     //  navBar.style.display = 'block';
+//     // }
+ 
+//     // else{
+//     //  navBar.style.display = 'none';
+//     // }
+// }
